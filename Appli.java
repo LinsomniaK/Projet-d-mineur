d9dbee7c6a8127b5159badbebd2166278f5541cc
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2016-05-11 14:57:22 +0200

    création classe Appli

---
+++ Appli.java
@@ -0,0 +1,17 @@
+package Demineur;
+
+
+
+public class Appli {
+
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                Model model = new Model();
+                ControlGroup controler = new ControlGroup(model);
+            }
+        });
+
+    }
+}
